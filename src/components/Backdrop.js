e20964ee096064b89dbc614a5c80a1189d726c53
Gleb Fedchenko <fedchenkogleb@gmail.com>
2018-02-22 23:26:27 +0200

    Delete Backdrop.js

--- src/components/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Backdrop = ({ show, clicked }) => {
-    return (
-        show ? <div onClick={clicked} className='Backdrop'></div> : null
-    )
-}
-
-export default Backdrop
